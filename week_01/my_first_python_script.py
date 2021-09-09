f30b9c5dc4d374f643dd0d827425a48073bb6ddd
Joseph Yurko <jyurko@gmail.com>
2021-09-09 10:36:39 -0400

    added week 01

---
+++ week_01/my_first_python_script.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+2 + 2
+
+3 + 3
+
+4 + 2
+
+5 / 7
+
+x = 2
+
+y = 3
+
+x * y
+
+z = x ** y
+
+# a comment is initiated with the hashtag
+# for us to describe what we're doing and why we're doing it
+
+## 
+
+### my preferred comment character
+
+# x + 3 this would normally not execute
+
+x + 3
+
+
+x + 3 # this however runs correctly
+
